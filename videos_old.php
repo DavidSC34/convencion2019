c698f580f4d33870c19e013a1768076ec3060f78
DavidSC <david@wbcboxing.com>
2019-11-19 11:37:11 -0600

    cambio en titulo de script de videos script old

--- videos_old.php
+++ videos_old.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>WBC Convention Videos</title>
+  <title>WBC Convention Videos Gallery</title>
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
